83226ced3e3aae41b9241a0c858f26c02adbd862
emre <122545769+Emreay24@users.noreply.github.com>
2024-02-20 23:08:53 +0300

    odev 2
    java101 odev 2

---
+++ Kdv.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Scanner;
+public class Kdv {
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+        double orjınal=0, kdv=0;
+        System.out.println("lütfen ücreti giriniz:");
+        orjınal=input.nextDouble();
+        kdv=orjınal;
+        if (orjınal<=1000 && orjınal>0){
+            kdv += (orjınal*0.18);
+            System.out.println("orjinal ucret: "+orjınal+" ve %8 KDV eklenerek genel ücret: "+kdv);
+        }
+        else{
+            kdv += (orjınal*0.8);
+            System.out.println("orjinal ucret: "+orjınal+" ve %8 KDV eklenerek genel ücret: "+kdv);
+
+        }
+    }
+}
