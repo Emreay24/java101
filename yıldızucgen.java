c45578f1d580fb735293ddaf0d94f914ddf9407f
Emreay24 <122545769+Emreay24@users.noreply.github.com>
2024-02-25 11:26:18 +0300

    yıldz ile üçgen yapma

---
+++ yıldızucgen.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+public class yıldızucgen {
+    public static void main(String[] args){
+        int n;
+        Scanner input=new Scanner(System.in);
+        System.out.print("lütfen bir sayı giriniz");
+        n=input.nextInt();
+        for(int i=1; i <=n; i++){
+            for (int k=1; k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(2*i)-1;j++) {
+                System.out.print("*");
+            }System.out.println();
+        }
+
+
+    }
+}
